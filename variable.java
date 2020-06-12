9c9be84ce93834ffcf0afe6382e272d9e9fe3dcc
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2020-06-12 19:59:19 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author abhis
+ */
+public class variable 
+{
+    public static void main(String[] arg)
+    {
+        int MyNum = 5;
+        float MyFloat = 5.99f;
+        char MyLetter = 'a';
+        boolean MyBool = true;
+        String MyText = "abhishek";
+        
+        System.out.println(MyNum);
+        System.out.println(MyFloat);
+        System.out.println(MyLetter);
+        System.out.println(MyBool);
+        System.out.println(MyText);
+        
+    }
+}
