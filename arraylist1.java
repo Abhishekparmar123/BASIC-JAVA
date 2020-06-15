c332c106bad7b8898d05790e81da0d8c8264d9f5
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2020-06-15 10:19:35 +0530

    define how to create a array list

--- arraylist1.java
+++ arraylist1.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication1;
 import java.util.ArrayList;
 import java.util.Scanner;
-/**
- *
- * @author abhis
- */
 public class arraylist1 
 {
     public static void main(String[] args)
