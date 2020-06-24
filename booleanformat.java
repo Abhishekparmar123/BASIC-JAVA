7d300b3194d18d4c511d2344293fa7d89dfce9d8
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2020-06-24 10:11:05 +0530

    This program to understand boolean operation

---
+++ booleanformat.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author abhis
+ */
+public class booleanformat 
+{
+    public static void main(String[] args)
+    {
+        boolean IsJavaFun = true;
+        boolean IsFishTasty = false;
+        System.out.println(IsJavaFun);
+        int x = 10;
+        int y = 5;
+        System.out.println(x>y);
+        System.out.println(x==y);
+    }
+}
