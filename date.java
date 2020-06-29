1a7763dc9dda935c68e91b9a855827a13145b140
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2020-06-29 11:14:00 +0530

    This program  is give date and time as output

--- date.java
+++ date.java
@@ -3,11 +3,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
  
 /**
  *
