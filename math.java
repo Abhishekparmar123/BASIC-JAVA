a616e5143893f5b483e37e303c87cc352f31bb2a
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2020-07-29 10:17:39 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author abhis
+ */
+public class math 
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 5;
+    
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.sqrt(a));
+        System.out.println(Math.abs(-4.56));
+    }
+}
