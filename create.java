1bfdcab400bbab4d9a51130a7c2779871a0e4fa5
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2020-07-29 10:26:57 +0530

    This program create a file in java

--- create.java
+++ create.java
@@ -3,11 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package file;
+import java.io.File;
+import java.io.IOException;
 /**
  *
  * @author abhis
  */
-public class create {
-
+public class create 
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            File obj = new File("exercise.txt");
+            if(obj.createNewFile())
+            {
+                System.out.println("file created ....");
+            }
+            else
+            {
+                System.out.println("File already exists ....");
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("an error occure ....");
+            e.printStackTrace();
+        }
+    }
 }
